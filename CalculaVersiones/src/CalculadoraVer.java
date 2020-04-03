2d12336abd3e55bf1ce8ea4eb6e0fcd8a2ecda0c
Ander Ezekiel <anderezekiel@gmail.com>
2020-04-03 12:37:07 +0200

    Creaccion de la clase CalculadoraVer y creaccion de sus dos constructores

---
+++ CalculaVersiones/src/CalculadoraVer.java
@@ -0,0 +1,17 @@
+
+public class CalculadoraVer {
+
+	private int num1,num2;
+
+	public CalculadoraVer(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	
+	public CalculadoraVer(int num1){
+		this.num1 = num1;
+	}
+	
+	
+}
